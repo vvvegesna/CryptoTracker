bfae57e28cc73abfb6be37023cb0ab1b8eb28b4c
Vegesna, Vijay Varma <vijayv.vegesna@iqvia.com>
2022-10-01 20:39:57 -0700

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Vegesna, Vijay Varma on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
